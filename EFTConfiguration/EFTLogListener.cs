6f21b090dbbe9449a9221635ca066a7e841d46e1
kmyuhkyuk <2451614940@qq.com>
2023-08-22 23:21:51 +0800

    Update

--- EFTConfiguration/EFTLogListener.cs
+++ EFTConfiguration/EFTLogListener.cs
@@ -24,17 +24,19 @@ namespace EFTConfiguration
             if (HideLog.Contains(logArg))
                 return;
 
-            var log = new LogData(sender, eventArgs);
-
-            AllLog.Add(log);
-
-            OnLog?.Invoke(log);
-
-            if (logArg.Contains(".Update ()"))
+            if (logArg.Contains(".Update ()") && (string)AllLog.Last().EventArgs.Data == logArg)
             {
                 LogSource.LogError("Major Error, This method loop throw error in Update (), Now added to hidden list, Please contact dev");
 
                 HideLog.Add(logArg);
+            }
+            else
+            {
+                var log = new LogData(sender, eventArgs);
+
+                AllLog.Add(log);
+
+                OnLog?.Invoke(log);
             }
         }
 

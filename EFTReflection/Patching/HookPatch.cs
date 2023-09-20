bb69a8fed852150037a6d5ff9529a8c68bf44aab
kmyuhkyuk <2451614940@qq.com>
2023-09-20 13:45:26 +0800

    Fixed HookPatch.NeedCastParameters wrong ignore base type

--- EFTReflection/Patching/HookPatch.cs
+++ EFTReflection/Patching/HookPatch.cs
@@ -243,9 +243,8 @@ namespace EFTReflection.Patching
                     (parameterName.StartsWith("___", StringComparison.Ordinal) &&
                      originalDeclaringType.GetField(parameterName.Remove(0, 3), AccessTools.all) != null) ||
                     originalParameters.Any(x =>
-                        x.Name == parameterName && (x.ParameterType == hookDelegateParameter.ParameterType ||
-                                                    x.ParameterType.IsAssignableFrom(
-                                                        hookDelegateParameter.ParameterType))))
+                        x.Name == parameterName && (x.ParameterType.IsAssignableFrom(
+                                                        hookDelegateParameter.ParameterType) || x.ParameterType.IsSubclassOf(hookDelegateParameter.ParameterType))))
                 {
                     list.Add(delegateParameters[i]);
                 }

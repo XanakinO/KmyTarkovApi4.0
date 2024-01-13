20b45891c73b4192c1fe0e68a4f345ef57109a06
kmyuhkyuk <2451614940@qq.com>
2024-01-13 22:27:19 +0800

    Update

--- Build/Program.cs
+++ Build/Program.cs
@@ -15,23 +15,25 @@ namespace Build
             const string releasePath =
                 "R:\\Battlestate Games\\Client.0.13.5.3.26535\\BepInEx\\plugins\\kmyuhkyuk-EFTApi";
 
-            Copy.CopyAssembly(arg, "Release", baseDirectory, releasePath, new[]
-            {
-                "EFTApi",
-                "EFTUtils",
-                "EFTReflection",
-                "EFTConfiguration",
-                "HtmlAgilityPack",
-                "Crc32.NET",
-                "ConfigurationTest"
-            });
+            Copy.CopyAssembly(arg, "Release", baseDirectory, releasePath,
+                new[]
+                {
+                    "EFTApi", 
+                    "EFTUtils", 
+                    "EFTReflection", 
+                    "EFTConfiguration", 
+                    "HtmlAgilityPack", 
+                    "Crc32.NET",
+                    "ConfigurationTest"
+                });
 
             const string unityEditorPath = "C:\\Users\\24516\\Documents\\EFTConfiguration\\Assets\\Managed";
 
-            Copy.CopyAssembly(arg, "UNITY_EDITOR", baseDirectory, unityEditorPath, new[]
-            {
-                "EFTConfiguration"
-            });
+            Copy.CopyAssembly(arg, "UNITY_EDITOR", baseDirectory, unityEditorPath, 
+                new[]
+                {
+                    "EFTConfiguration"
+                });
         }
     }
 }

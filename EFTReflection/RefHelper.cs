890b2190ee400da4e3f3f800fa55cd241c382d4e
kmyuhkyuk <2451614940@qq.com>
2024-04-30 06:16:46 +0800

    Update

--- EFTReflection/RefHelper.cs
+++ EFTReflection/RefHelper.cs
@@ -560,7 +560,7 @@ namespace EFTReflection
                     _instance = (T)instance;
                 }
 
-                if (typeof(TF) == typeof(object) || (DeclaringType != null && DeclaringType.IsValueType) || typeof(TF).IsValueType)
+                if (typeof(TF) == typeof(object) || typeof(TF).IsValueType || (DeclaringType != null && DeclaringType.IsValueType))
                 {
                     _refGetValue = ObjectFieldGetAccess<T, TF>(fieldInfo);
                     _refSetValue = ObjectFieldSetAccess<T, TF>(fieldInfo);
